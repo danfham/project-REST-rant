38cd509e3bae04c77f8401f42a4b298ff78831bf
danfham <dan.cunningham91@icloud.com>
2023-07-12 22:33:25 -0400

    added 404 error page

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//middleware
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
+//routers
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+  res.render('error404')
 })
 
-app.listen(process.env.PORT)
 
+const PORT = process.env.PORT
+app.listen(PORT,console.log(`listening on port ${PORT}`))
