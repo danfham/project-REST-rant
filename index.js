60b804ec7d8ff749ef82e24149b8343fd69352c0
danfham <dan.cunningham91@icloud.com>
2023-07-19 18:26:47 -0400

    Adding Bootstrap grid to index page

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express')
 const app = express()
 
 //middleware
+app.set('views',__dirname+'/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 app.use('/places', require('./controllers/places'))
+app.use(express.urlencoded({ extended: true }))
+
 
 //routers
 app.get('/', (req, res) => {
